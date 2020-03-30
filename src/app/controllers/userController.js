fdad8388db98ef1ad207237e47abf531a8949bfd
RenatoShishido <renato.re2012@hotmail.com>
2020-03-29 23:44:22 -0400

    version 2.0     backend

--- src/app/controllers/userController.js
+++ src/app/controllers/userController.js
@@ -19,6 +19,19 @@ router.get('/', async (req, res) => {
   }
 })
 router.put('/update/:id', multer(multerConfig).single("file") , async (req, res) => {
+  function base64_decode(base64str,key){
+    var bitmap = new Buffer (base64str, 'base64');
+    fs.writeFileSync('src/temp/'+key+'',bitmap, 'binary', function (err){
+      if(err){
+        console.log('Conversao com erro');
+      }
+    } );
+  }
+  //Convertendo arquivo em binário
+  function base64_encode(key){
+    var bitmap = fs.readFileSync('tmp/uploads/'+key+'');
+    return new Buffer (bitmap).toString('base64');
+  }
   try {
     const {nome, email, telefone, rga, semestre} = req.body
     const id = req.params.id
@@ -26,21 +39,23 @@ router.put('/update/:id', multer(multerConfig).single("file") , async (req, res)
       const user = await User.findByIdAndUpdate(id, req.body)
 
       return res.send({ user })
+    }else if(req.file !== undefined || req.file !== null && req.body === undefined){
+      const {originalname: name, size, filename: key } = req.file
+     
+      const leitura = base64_encode(key)
+      const bitmap = "data:image/jpeg;image/png;image/pjpeg;base64," + leitura
+
+      const user = await User.findByIdAndUpdate(id,{
+        name,
+        size,
+        key,
+        url: '',
+        profile: bitmap})
+
+      return res.send({ user })
     }else{
       const {originalname: name, size, filename: key } = req.file
-      function base64_decode(base64str,key){
-        var bitmap = new Buffer (base64str, 'base64');
-        fs.writeFileSync('src/temp/'+key+'',bitmap, 'binary', function (err){
-          if(err){
-            console.log('Conversao com erro');
-          }
-        } );
-      }
-      //Convertendo arquivo em binário
-      function base64_encode(key){
-        var bitmap = fs.readFileSync('tmp/uploads/'+key+'');
-        return new Buffer (bitmap).toString('base64');
-      }
+     
         const leitura = base64_encode(key)
         const bitmap = "data:image/jpeg;image/png;image/pjpeg;base64," + leitura
 
