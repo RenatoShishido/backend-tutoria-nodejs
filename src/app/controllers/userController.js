d534e59458e66223e74b2c1b396392f3284abd65
RenatoShishido <renato.re2012@hotmail.com>
2020-04-03 02:27:35 -0400

    Backend funcionando

--- src/app/controllers/userController.js
+++ src/app/controllers/userController.js
@@ -18,6 +18,17 @@ router.get('/', async (req, res) => {
     res.status(500).send({error: "Erro ao listar todos os usuarios"})
   }
 })
+router.get('/perfil/:perfilId', async (req, res) => {
+  try {
+    const id = req.params.perfilId
+    const user = await User.findById(id)
+
+    return res.send({ user })
+  } catch (err) {
+    console.log(err)
+    res.status(500).send({error: "Erro ao listar todos os usuarios"})
+  }
+})
 router.delete('/remove/:id', async (req, res) => {
   try {
 
@@ -69,20 +80,7 @@ router.put('/update/:id', multer(multerConfig).single("file") , async (req, res)
       const user = await User.findByIdAndUpdate(id, req.body)
 
       return res.send({ user })
-    }else if(req.file !== undefined || req.file !== null && req.body === undefined){
-      const {originalname: name, size, filename: key } = req.file
-     
-      const leitura = base64_encode(key)
-      const bitmap = "data:image/jpeg;image/png;image/pjpeg;base64," + leitura
-
-      const user = await User.findByIdAndUpdate(id,{
-        name,
-        size,
-        key,
-        url: '',
-        profile: bitmap})
-
-      return res.send({ user })
+    
     }else{
       const {originalname: name, size, filename: key } = req.file
      
