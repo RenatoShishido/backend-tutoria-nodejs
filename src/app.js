ac9bbc41b1ff5d1c508074663c38cfc4f2460fe4
Renato <renato.re2012@hotmail.com>
2021-04-18 17:47:47 -0400

    Testando heroku

--- src/app.js
+++ src/app.js
@@ -7,6 +7,10 @@ const path = require('path')
 
 require("./database/index")
 
+app.get("/", (req, res) => {
+    res.send("Hello World")
+})
+
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false}))
 app.use(bodyParser.json())
